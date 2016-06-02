0837a7f885cb0cb35075b455d0a3fa84b67fdd09
Arun Sood <arsood@gmail.com>
2016-06-02 13:48:57 -0700

    Added SoundCloud

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
+    SC.initialize({
+        client_id: 'db46e7bcfb5e2a36618d46e89a14eb1e'
+    });
+
     $(document).on("click", "#search-submit", function() {
+        //Standard AJAX for Spotify
         $.ajax({
             type: "GET",
             url: "https://api.spotify.com/v1/search?type=track&limit=5&q=" + encodeURIComponent($("#song-search").val()),
@@ -16,7 +21,7 @@ $(document).ready(function() {
                 });
 
                 //Display the search query on the UI
-                $("#display-for").html('for "' + $("#song-search").val() + '"');
+                $(".display-for").html('for "' + $("#song-search").val() + '"');
 
                 //Fade in the results when all is done
                 $("#results-container").fadeIn();
@@ -24,6 +29,19 @@ $(document).ready(function() {
             error: function() {
                 alert("Sorry, your query returned no results.");
             }
+        });
+
+        //Using JavaScript SDK for SoundCloud
+        SC.get('/tracks', {
+            q: $("#song-search").val(),
+            limit: 5
+        }).then(function(scTracks) {
+            HANDLE.renderTemplate({
+                templateSource: "#soundcloud-template",
+                data: scTracks,
+                where: "#soundcloud-results",
+                clearOriginal: true
+            });
         });
     });
 
